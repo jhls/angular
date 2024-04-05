d7c95478b02b548569194e3e8ed1b2e493480e2d
Jean Lacerda <jean.sena@sicoob.com.br>
2024-04-04 22:49:45 -0300

    Criação de rotas

--- rotas/rotas/src/app/app.module.ts
+++ rotas/rotas/src/app/app.module.ts
@@ -6,18 +6,23 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { CursosComponent } from './cursos/cursos.component';
 import { AppRoutingModule } from './app-routing.module';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import { CursosService } from './cursos/cursos.service';
+import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    CursosComponent
+    CursosComponent,
+    CursoDetalheComponent,
+    CursoNaoEncontradoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [CursosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ rotas/rotas/src/app/cursos/cursos.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CursosService {
+
+  constructor() { }
+
+  getCursos(){
+    return [
+      {id:1, nome: 'Angular 2'},
+      {id:2, nome: 'Java'}
+    ];
+  }
+
+  getCurso(id: number){
+    let cursos = this.getCursos();
+    return cursos.find(curso => curso.id == id);
+  }
+}
