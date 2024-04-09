c4956dfa3dd5456490d00d00cd4173e0b94de8d8
Jean Lacerda <jean.sena@sicoob.com.br>
2024-04-08 21:06:02 -0300

    Refatorando por módulos e criação do componente de alunos

--- rotas/rotas/src/app/app.module.ts
+++ rotas/rotas/src/app/app.module.ts
@@ -1,28 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { CursosComponent } from './cursos/cursos.component';
-import { AppRoutingModule } from './app-routing.module';
-import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
-import { CursosService } from './cursos/cursos.service';
-import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
+import { CursosModule } from './cursos/cursos.module';
+import { AlunosComponent } from './alunos/alunos.component';
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     LoginComponent,
-    CursosComponent,
-    CursoDetalheComponent,
-    CursoNaoEncontradoComponent
+    AlunosComponent
   ],
   imports: [
     BrowserModule,
+    CursosModule,
     AppRoutingModule
   ],
-  providers: [CursosService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
