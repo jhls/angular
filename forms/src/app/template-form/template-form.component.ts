b07be551ae09c96e6caec42e613d8f526d5bc7ef
Jean Lacerda <jean.sena@sicoob.com.br>
2024-04-25 21:50:39 -0300

    Criação de componente de exibição de erros

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -22,4 +22,16 @@ export class TemplateFormComponent implements OnInit {
     console.log(this.usuario);
   }
 
+  verificaValidTouched(campo: any){
+    return !campo.valid && campo.touched;
+  }
+
+  aplicaCssErro(campo:any){
+    return {
+        'has-error': this.verificaValidTouched(campo),
+        'has-feedback': this.verificaValidTouched(campo)
+    }
+  }
+  
+
 }
