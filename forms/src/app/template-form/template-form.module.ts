e560b2fecbe41e8ecefc1d17c539e50e761af917
Jean Lacerda <jean.sena@sicoob.com.br>
2024-04-26 22:53:52 -0300

    Ajustes e primeiro form reativo

--- forms/src/app/template-form/template-form.module.ts
+++ forms/src/app/template-form/template-form.module.ts
@@ -2,22 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { TemplateFormComponent } from './template-form.component';
-import { FormDebugComponent } from '../form-debug/form-debug.component';
-import { CampoControlErroComponent } from '../campo-control-erro/campo-control-erro.component';
 import { HttpClientModule } from '@angular/common/http';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
   declarations: [
-    TemplateFormComponent,
-    FormDebugComponent,
-    CampoControlErroComponent
+    TemplateFormComponent
   ],
   imports: [
     CommonModule,
     FormsModule,
     HttpClientModule,
-    
+    SharedModule
   ]
 })
 export class TemplateFormModule { }
